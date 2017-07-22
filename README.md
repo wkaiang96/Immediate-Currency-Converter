d3fff7a9ac85a0852505bcbbb65f2164d041cb56
Yong Jia <yjtoro@hotmail.com>
2017-07-22 16:38:09 +0800

    Update 0.2

--- README.md
+++ README.md
@@ -9,13 +9,21 @@
 
 #For Reviewers
 
-###### This plugin required jquery to works. The jquery version I'm using is 3.2.1 (non-minified version). 
+###### This plugin required jquery to works. The jquery version I'm using is 3.2.1 (non-minified version).
 
 ###### The jquery file has been included in the extension, in case you would to reproduce it, copy the whole jquery-3.2.1 and make a new file again.
 
-###### The currency rate that I'm getting is from http://www.fixer.io. Visit their site to know about it.
+###### The currency rate that I'm getting is from https://www.fixer.io. Visit their site to know about it.
 
 ###### *I know I have written a poor readme, contact me if doubt, yjtoro@hotmail.com*
+
+##Update 0.1
+###### Added support for EUR currencies
+###### Reviewer kindly look at background.js with comment updated field
+
+##Update 0.2
+###### User hover to see currency's country / locate.
+###### Able to highlight and convert amount with comma ','. Example: 1,000,000.00 is accepted.
 ====================================================
 # Immediate-Currency-Converter
 Highlight number on web page and instantly convert to desired currency
--- background.js
+++ background.js
@@ -5,20 +5,27 @@ var user_base = "undefined";
 var user_convert = "undefined";
 var title = '';
 var dailyData = '';
-
+//updated field
+var dailyDataArray;
 function init(){
   $.getJSON("http://api.fixer.io/latest", storeDailyData);
 }
 
 var storeDailyData = function(data){
   dailyData = data;
+  console.log(JSON.stringify(data));
+  var obj = dailyData.rates;
+  var result = Object.keys(obj).map(function(e) {
+    return [(e), obj[e]];
+  });
+  dailyDataArray = result;
 }
 
 init();
 
 var makeTitle = function(){
     let gettingItem = browser.storage.local.get();
-    gettingItem.then(onGot, onError);    
+    gettingItem.then(onGot, onError);
 }
 
 function onGot(item){
@@ -62,23 +69,64 @@ browser.contextMenus.onClicked.addListener(function(info, tab) {
 function onGotConvert(item){
   console.log(item);
   var emptyObj = isEmpty(item);
-  if((!emptyObj)){  
+  if((!emptyObj)){
     if(item.base_curr.name && item.convert_curr.name){
       base_curr = item.base_curr.name;
       convert_curr = item.convert_curr.name;
-      if(base_curr && convert_curr){
-        fx.rates = dailyData.rates;
-        var rate = fx(user_amount).from(base_curr).to(convert_curr);
-        console.log(base_curr + " " + user_amount + " = " + convert_curr + " " + rate.toFixed(2));
-      
-        browser.notifications.create({
-          "type": "basic",
-          "iconUrl": browser.extension.getURL("icons/curr_converter-100.png"),
-          "title": "Immediate Currency Converter",
-          "message": base_curr + " " + user_amount + " = " + convert_curr + " " + rate.toFixed(2)
-        });
+
+      //Update 0.1, new field for eur currencies
+      if(base_curr === "EUR" || convert_curr === "EUR"){
+        if(base_curr === "EUR"){
+          var convert_rate;
+          var converted_amount;
+          for(var n = 0; n < dailyDataArray.length; n++){
+            if(convert_curr === dailyDataArray[n][0]){
+              convert_rate = dailyDataArray[n][1];
+              converted_amount = user_amount * convert_rate;
+              converted_amount = Math.round(converted_amount * 100) / 100;
+            }
+          }
+          browser.notifications.create({
+            "type": "basic",
+            "iconUrl": browser.extension.getURL("icons/curr_converter-100.png"),
+            "title": "Immediate Currency Converter",
+            "message": base_curr + " " + user_amount + " = " + convert_curr + " " + converted_amount
+          });
+          console.log(convert_rate);
+        }
+        if(convert_curr === "EUR"){
+          var convert_rate;
+          var converted_amount;
+          for(var n = 0; n < dailyDataArray.length; n++){
+            if(base_curr === dailyDataArray[n][0]){
+              convert_rate = dailyDataArray[n][1];
+              converted_amount = user_amount / convert_rate;
+              converted_amount = Math.round(converted_amount * 100) / 100;
+            }
+          }
+          browser.notifications.create({
+            "type": "basic",
+            "iconUrl": browser.extension.getURL("icons/curr_converter-100.png"),
+            "title": "Immediate Currency Converter",
+            "message": base_curr + " " + user_amount + " = " + convert_curr + " " + converted_amount
+          });
+        }
+        //Update 0.1 end here.
+      }else{
+        if(base_curr && convert_curr){
+          fx.rates = dailyData.rates;
+          var rate = fx(user_amount).from(base_curr).to(convert_curr);
+          console.log(base_curr + " " + user_amount + " = " + convert_curr + " " + rate.toFixed(2));
+
+          browser.notifications.create({
+            "type": "basic",
+            "iconUrl": browser.extension.getURL("icons/curr_converter-100.png"),
+            "title": "Immediate Currency Converter",
+            "message": base_curr + " " + user_amount + " = " + convert_curr + " " + rate.toFixed(2)
+          });
+        }
       }
-    } 
+    }
   }else{
     fx.rates = data.rates;
     var rate = fx(user_amount).from(base_curr).to(convert_curr);
@@ -98,12 +146,24 @@ function currency_convert() {
   let get_user_base = browser.storage.local.get();
   get_user_base.then(onGotConvert, onError);
 }
-
+/**
+*Updates 0.2 to accept 100,000.00 kind of amount.
+*/
 function convert(variable){
    var amount = variable;
    if(! isNaN(amount)){
       user_amount = Number(amount);
       currency_convert();
+   }else if(amount.includes(',')){
+     var str_amount = amount;
+     while (str_amount.includes(',')) {
+       str_amount = str_amount.replace(',','');
+     }
+     user_amount = Number(str_amount);
+     currency_convert();
+/**
+*Update 0.2 end here.
+*/
    }else{
      browser.notifications.create({
       "type": "basic",
